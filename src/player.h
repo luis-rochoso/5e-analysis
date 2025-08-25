a8a6276e5fae2940236730eb949d93b807f0edba
luis-rochoso <sousaluislfs@gmail.com>
2025-08-25 12:27:17 -0300

    Added 'null' class and race'

--- src/player.h
+++ src/player.h
@@ -1,10 +1,10 @@
 enum Class {
-    artificer, barbarian, bard, cleric, druid, fighter, monk,
+    null, artificer, barbarian, bard, cleric, druid, fighter, monk,
     paladin, ranger, rogue, sorcerer, warlock, wizard
 };
 
 enum Race {
-    dragonborn, dwarfHill, dwarfMountain, elfDrow, elfHigh, elfWood,
+    null, dragonborn, dwarfHill, dwarfMountain, elfDrow, elfHigh, elfWood,
     gnomeForest, gnomeRock, halfElf, halfOrc, halflingLightfoot,
     halflingStout, human, tiefling
 };
