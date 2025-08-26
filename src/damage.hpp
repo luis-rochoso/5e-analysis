917e3dcdfe101ec521878a9c032f80c79e9d7025
LuisaFS <luisaferreirass08@gmail.com>
2025-08-26 12:15:53 -0300

    Added 'damage' header file, changed header format from .h to .hpp

---
+++ src/damage.hpp
@@ -0,0 +1,26 @@
+#ifndef DAMAGE_H
+#define DAMAGE_H
+
+enum DmgType {
+    acid, bludgeoning, cold, fire, force, lightning, necrotic,
+    piercing, poison, psychic, radiant, slashing, thunder
+};
+
+struct Damage {
+    short value;
+    DmgType type;
+    bool magic {false};
+};
+
+class Attacker {
+    short toHit;
+    Damage dmg;
+};
+
+class Saver {
+    short cd;
+    short targets {1};
+    Damage dmg;
+};
+
+#endif
